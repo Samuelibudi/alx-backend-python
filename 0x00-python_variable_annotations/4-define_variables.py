322e19eb229847379cf36b52a49e6c584d4c7cb9
Samuelibudi <ibudisamuel@gmail.com>
2023-07-06 16:59:16 +0000

    Adding file

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+This module defines and annotates variables
+"""
+
+a = 1  # An nteger with value of 1
+pi = 3.14  # A float with a value of 3.14
+i_understand_annotations = True  # A boolen with a value of True
+school = "Holberton"  # A string with a value of "Holberton"
