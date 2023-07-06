edb84ed96404ace0587f8826e17aeab44bc5cccf
Samuelibudi <ibudisamuel@gmail.com>
2023-07-06 17:26:39 +0000

    Fixing errors

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -4,7 +4,7 @@
 This module defines and annotates variables
 """
 
-a = 1  # An nteger with value of 1
-pi = 3.14  # A float with a value of 3.14
-i_understand_annotations = True  # A boolen with a value of True
-school = "Holberton"  # A string with a value of "Holberton"
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
