ec9b759689e3e3093a0387138b41325cab6ab0d7
Samuelibudi <ibudisamuel@gmail.com>
2023-07-06 17:10:58 +0000

    Adding file

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+A function that takes a list and returns sum of list.
+"""
+
+
+def sum_list(input_list: list) -> float:
+    """
+    A function that takes a list parameters and returns
+    sum.
+
+    @Params:
+        input_list (list): List argument 
+
+    Returns:
+        float: Returning value
+
+    """
+    return sum(input_list)
